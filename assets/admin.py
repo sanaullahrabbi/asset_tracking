096f36d9a134e23563eb69be2659eb6a34d5ed96
Sanaullah Rabbi <srrabbi01@gmail.com>
2024-03-29 23:21:18 +0600

    create models and api

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Company, Employee, Device, DeviceLog
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name",)  # Display additional fields if needed
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "company",
+        "phone",
+        "designation",
+    )
+    search_fields = ["name", "company__name", "phone"]
+    list_filter = ("company",)
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(DeviceLog)
+class DeviceLogAdmin(admin.ModelAdmin):
+    list_display = ("device", "employee", "check_out_time", "check_in_time")
+    list_filter = ("device", "check_out_time", "check_in_time")
+    search_fields = ("device__name", "employee__name")
--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="employees"
+    )
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=16)
+    designation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} (works at {self.company.name})"
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    serial_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name="logs")
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, related_name="device_logs"
+    )
+    check_out_time = models.DateTimeField(default=timezone.now)
+    check_in_time = models.DateTimeField(null=True, blank=True)
+    check_out_condition = models.CharField(max_length=100)
+    check_in_condition = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.device.name} - {self.employee.name}"
